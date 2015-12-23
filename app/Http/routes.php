a69138d619f83cb862ae010ce820aaee251273a9
billy <william.allan@glasgow.ac.uk>
2015-12-23 17:04:49 +0000

    First bits of code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+Route::group(['middleware' => 'web'], function () {
 
-Route::group(['middleware' => ['web']], function () {
-    //
+    Route::auth();
+
+    Route::group(['prefix' => 'dhcp'], function () {
+        Route::get('/', 'DhcpController@index');
+        Route::get('/create', 'DhcpController@create');
+        Route::post('/create', 'DhcpController@store');
+        Route::get('/edit/{id}', 'DhcpController@edit');
+        Route::post('/edit/{id}', 'DhcpController@update');
+        Route::delete('/delete/{id}', 'DhcpController@destroy');
+        Route::get('/search/{term}', 'DhcpController@search');
+    });
+
+    Route::group(['prefix' => 'license'], function () {
+        Route::get('/', 'LicenseController@index');
+        Route::get('/create', 'LicenseController@create');
+        Route::post('/create', 'LicenseController@store');
+        Route::get('/edit/{id}', 'LicenseController@edit');
+        Route::post('/edit/{id}', 'LicenseController@update');
+        Route::delete('/delete/{id}', 'LicenseController@destroy');
+        Route::get('/search/{term}', 'LicenseController@search');
+    });
+
+    Route::group(['prefix' => 'ipseen'], function () {
+        Route::get('/', 'IpController@index');
+        Route::get('/search/{term}', 'IpController@search');
+    });
 });
